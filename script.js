41fad65b465619682b84e2bcd3206543c0b1791b
Dragos Neacsu <dragosncs@gmail.com>
2021-05-15 11:03:55 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,152 @@
+
+const countriesEl = document.getElementById('countries');
+const toggleBtn = document.getElementById('toggle');
+const filterBtn = document.getElementById('filter');
+const regionFilters = filterBtn.querySelectorAll('li');
+const searchEl = document.getElementById('search');
+const modal = document.getElementById('modal');
+const closeBtn = document.getElementById('close');
+
+getCountries();
+
+async function getCountries() {
+	const res = await fetch('https://restcountries.eu/rest/v2/all');
+	const countries = await res.json();
+
+	displayCountries(countries);
+}
+
+function displayCountries(countries) {
+	countriesEl.innerHTML = '';
+
+	countries.forEach(country => {
+		const countryEl = document.createElement('div');
+		countryEl.classList.add('card');
+
+		countryEl.innerHTML = `
+            <div>
+                <img src="${country.flag}" alt="Germany" />
+            </div>
+            <div class="card-body">
+                <h3 class="country-name">${country.name}</h3>
+                <p class="contry-population">
+                    <strong>Population:</strong>
+                    ${country.population}
+                </p>
+                <p class="country-region">
+                    <strong>Region:</strong>
+                    ${country.region}
+                </p>
+                <p>
+                    <strong>Capital:</strong>
+                    ${country.capital}
+                </p>
+            </div>
+        `;
+
+		countryEl.addEventListener('click', () => {
+			modal.style.display = 'flex';
+			showCountryDetails(country);
+		});
+
+		countriesEl.appendChild(countryEl);
+	});
+}
+
+function showCountryDetails(country) {
+	const modalBody = modal.querySelector('.modal-body');
+	const modalImg = modal.querySelector('img');
+
+	modalImg.src = country.flag;
+
+	modalBody.innerHTML = `
+        <h2>${country.name}</h2>
+        <p>
+            <strong>Population:</strong>
+            ${country.population}
+        </p>
+        <p>
+            <strong>Region:</strong>
+            ${country.region}
+        </p>
+        <p>
+            <strong>Alpha 2 Code:</strong>
+            ${country.alpha2Code}
+        </p>
+        <p>
+            <strong>Area:</strong>
+            ${country.area}
+        </p>
+        <p>
+            <strong>Capital:</strong>
+            ${country.capital}
+        </p>
+        <p>
+            <strong>Time zone:</strong>
+            ${country.timezones}
+        </p>
+        <p>
+            <strong>Latnlg:</strong>
+            ${country.latlng}
+        </p>
+        <p>
+            <strong>CurrentTime:</strong>
+            ${country.Currenttime}  <! Time not specified in Rest API>
+        </p>
+        <p>
+            <strong>Neighbours:</strong>
+            ${country.borders}
+        </p>
+        <p>
+            <strong>Currencies:</strong>
+            ${country.currencies.map(currency => currency.code)}
+        </p>
+        <p>
+            <strong>Languages:</strong>
+            ${country.languages.map(language => language.name)}
+        </p>
+        
+    `;
+}
+
+
+
+// display filters 
+filterBtn.addEventListener('click', () => {
+	filterBtn.classList.toggle('open');
+});
+
+closeBtn.addEventListener('click', () => {
+	modal.style.display = 'none';
+});
+
+searchEl.addEventListener('input', e => {
+	const { value } = e.target;
+	const countryName = document.querySelectorAll('.country-name');
+
+	countryName.forEach(name => {
+		if (name.innerText.toLowerCase().includes(value.toLowerCase())) {
+			// .card -> .card-body -> .country-name
+			name.parentElement.parentElement.style.display = 'block';
+		} else {
+			name.parentElement.parentElement.style.display = 'none';
+		}
+	});
+});
+
+
+regionFilters.forEach(filter => {
+	filter.addEventListener('click', () => {
+		const value = filter.innerText;
+		const countryRegion = document.querySelectorAll('.country-region');
+
+		countryRegion.forEach(region => {
+			if (region.innerText.includes(value) || value === 'All') {
+				// .card -> .card-body -> .country-region
+				region.parentElement.parentElement.style.display = 'block';
+			} else {
+				region.parentElement.parentElement.style.display = 'none';
+			}
+		});
+	});
+});
